0f773322a7407652cf7713a661fc6625b72c8466
현지현 <qwere4585@naver.com>
2024-12-17 21:48:30 +0900

    feat: add main path in router

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import MainPage from './pages/main/MainPage';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([{ path: '/', element: <MainPage /> }]);
 
 export default router;
