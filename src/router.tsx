3a6499d3f3a56066ce678370f5ba64be9c1693bf
현지현 <qwere4585@naver.com>
2024-12-18 17:55:51 +0900

    feat: 카테고리 페이지 라우팅 추가, 페이지 생성

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/main/MainPage';
 import CategoryRegisterPage from './pages/todo/category/CategoryRegisterPage';
+import CategoryPage from './pages/todo/category/CategoryPage';
 
 const router = createBrowserRouter([
     { path: '/', element: <MainPage /> },
+    { path: '/todo/category', element: <CategoryPage /> },
     { path: '/todo/category/new', element: <CategoryRegisterPage /> },
 ]);
 
