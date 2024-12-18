02eee105343bf199635b2c1f182461d17d342b8a
kkyung99 <becky0508@naver.com>
2024-12-18 15:35:03 +0900

    feat: add routes for LetterWritePage and ReflectWritePage

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import LetterWritePage from './pages/timecapsule/LetterWritePage';
+import ReflectWritePage from './pages/timecapsule/ReflectWritePage';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/write/letter',
+		element: <LetterWritePage />,
+	},
+	{
+		path: '/write/reflect',
+		element: <ReflectWritePage />,
+	},
+]);
 
 export default router;
