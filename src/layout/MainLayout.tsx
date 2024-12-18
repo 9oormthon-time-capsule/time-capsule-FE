5b201b26c7f7e6a6b3041432eee3b6cde141eb84
ccconac <qwerpoiu91@naver.com>
2024-12-18 15:30:40 +0900

    design: 타임 캡슐 보관함 UI 구현

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+function MainLayout({ children }: { children: React.ReactNode }) {
+  return <Container>{children}</Container>;
+}
+
+export default MainLayout;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 1200px;
+  min-width: 700px;
+
+  margin: 0 auto;
+`;
