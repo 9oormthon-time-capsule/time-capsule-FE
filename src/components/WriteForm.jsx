18589fe2f2a534a3e5f87cbbb4494669514729d9
ccconac <qwerpoiu91@naver.com>
2024-12-19 15:03:01 +0900

    feat: 작성하기 버튼 클릭 시 캡슐 보관 페이지로 이동

--- src/components/WriteForm.jsx
+++ src/components/WriteForm.jsx
@@ -3,10 +3,26 @@ import { Form, TextArea } from '../styles/WriteForm';
 import CustomButton from './CustomButton';
 
 export default function LetterForm({ placeholder }) {
-	return (
-		<Form>
-			<TextArea placeholder={placeholder} required />
-			<CustomButton type="submit" text={'작성하기'} />
-		</Form>
-	);
+  const currentPath = window.location.pathname;
+
+  const handleWriteButton = (event) => {
+    event.preventDefault();
+
+    if (currentPath === '/write/letter') {
+      window.location.href = '/directory/time/1';
+    } else {
+      window.location.href = '/directory/diary/1';
+    }
+  };
+
+  return (
+    <Form>
+      <TextArea placeholder={placeholder} required />
+      <CustomButton
+        onClick={handleWriteButton}
+        type="submit"
+        text={'작성하기'}
+      />
+    </Form>
+  );
 }
