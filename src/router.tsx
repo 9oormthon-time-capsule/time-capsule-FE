6e56827250e1b048cecb652c835ac43678e34f07
ccconac <qwerpoiu91@naver.com>
2024-12-19 11:23:15 +0900

    feat: 로그인 페이지 라우터 추가

--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,14 @@ import MainPage from './pages/main/MainPage';
 import Directory from './pages/timecapsule/Directory';
 import LetterWritePage from './pages/timecapsule/LetterWritePage';
 import ReflectWritePage from './pages/timecapsule/ReflectWritePage';
+import Login from './pages/login/Login';
 
 const router = createBrowserRouter([
-	{ path: '/', element: <MainPage /> },
-	{ path: '/directory/:type/:userId', element: <Directory /> },
-	{ path: '/write/letter', element: <LetterWritePage /> },
-	{ path: '/write/reflect', element: <ReflectWritePage /> },
+  { path: '/auth', element: <Login /> },
+  { path: '/', element: <MainPage /> },
+  { path: '/directory/:type/:userId', element: <Directory /> },
+  { path: '/write/letter', element: <LetterWritePage /> },
+  { path: '/write/reflect', element: <ReflectWritePage /> },
 ]);
 
 export default router;
