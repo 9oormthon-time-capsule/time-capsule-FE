3bf4d23a7a35c98171ec911bb73a1abb1d8aa47b
ccconac <qwerpoiu91@naver.com>
2024-12-19 11:38:54 +0900

    fix: 로그인 및 메인 페이지 라우터 변경

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
+
+import Login from './pages/login/Login';
 import MainPage from './pages/main/MainPage';
 import Directory from './pages/timecapsule/Directory';
 import LetterWritePage from './pages/timecapsule/LetterWritePage';
 import ReflectWritePage from './pages/timecapsule/ReflectWritePage';
-import Login from './pages/login/Login';
 
 const router = createBrowserRouter([
-  { path: '/auth', element: <Login /> },
-  { path: '/', element: <MainPage /> },
+  { path: '/', element: <Login /> },
+  { path: '/main', element: <MainPage /> },
   { path: '/directory/:type/:userId', element: <Directory /> },
   { path: '/write/letter', element: <LetterWritePage /> },
   { path: '/write/reflect', element: <ReflectWritePage /> },
