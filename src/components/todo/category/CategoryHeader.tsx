7784af8199fd5a1d9b3fbae172f04eec547cac5d
현지현 <qwere4585@naver.com>
2024-12-19 15:26:35 +0900

    feat: 뒤로가기, 등록페이지 네비게이션 구현

--- src/components/todo/category/CategoryHeader.tsx
+++ src/components/todo/category/CategoryHeader.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CategoryHeader = ({ title, button }: { title: string; button: string }) => {
+	const nav = useNavigate();
+
+	const handleBackNav = () => {
+		nav(-1);
+	}
+
+	const toRegisterPage = () => {
+		nav('/todo/category/new');
+	}
+
 	return (
 		<Wrapper>
-			<BackSection>{'<'}</BackSection>
+			<BackSection onClick={handleBackNav}>{'<'}</BackSection>
 			<TitleSection>{title}</TitleSection>
-			<ButtonSection>{button}</ButtonSection>
+			<ButtonSection isPlus={button === '+'} onClick={toRegisterPage}>{button}</ButtonSection>
 		</Wrapper>
 	);
 };
@@ -15,20 +26,24 @@ const Wrapper = styled.div`
 	height: auto;
 	display: flex;
 	justify-content: space-between;
-	padding: 40px 150px;
+	align-items: center;
+	padding: 30px 180px;
 `;
 
 const BackSection = styled.div`
-	font-size: 25px;
+	font-size: 30px;
+	cursor: pointer;
 `;
 
 const TitleSection = styled.div`
-	font-size: 20px;
+	font-size: 23px;
 	font-weight: bold;
 `;
 
 const ButtonSection = styled.div`
-	font-size: 20px;
+	font-size: ${({ isPlus }) => (isPlus ? '40px' : '20px')};
+	font-weight: ${({ isPlus }) => (isPlus ? '400' : '600')};
+	cursor: pointer;
 `;
 
 export default CategoryHeader;
