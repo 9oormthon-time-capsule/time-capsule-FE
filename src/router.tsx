5f7b805a0abab88ab126118bc56fa5598dbe0c1a
ccconac <qwerpoiu91@naver.com>
2024-12-19 14:20:25 +0900

    feat: 타임 캡슐 상세 페이지 라우터 추가

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,8 @@ import MainPage from './pages/main/MainPage';
 import Directory from './pages/timecapsule/Directory';
 import LetterWritePage from './pages/timecapsule/LetterWritePage';
 import ReflectWritePage from './pages/timecapsule/ReflectWritePage';
+import LetterDetail from './pages/timecapsule/LetterDetail';
+import ReflectDetail from './pages/timecapsule/ReflectDetail';
 
 const router = createBrowserRouter([
   { path: '/', element: <Login /> },
@@ -12,6 +14,8 @@ const router = createBrowserRouter([
   { path: '/directory/:type/:userId', element: <Directory /> },
   { path: '/write/letter', element: <LetterWritePage /> },
   { path: '/write/reflect', element: <ReflectWritePage /> },
+  { path: '/detail/letter/:letterId', element: <LetterDetail /> },
+  { path: '/detail/reflect/:letterId', element: <ReflectDetail /> },
 ]);
 
 export default router;
