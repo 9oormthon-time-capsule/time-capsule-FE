9d25b90e89b167506285dd0905cdfc494e400187
kkyung <68538307+kkyung99@users.noreply.github.com>
2024-12-20 09:24:44 +0900

    [UI] design: todo 할 일 추가 컴포넌트 UI 구현 (#18)
    * feat: todo를 작성하는 input, 커스텀 checkbox 컴포넌트 구현
    
    * style: AddTodo 컴포넌트 스타일 수정
    
    * chore: yarn 설치 상태 업데이트

---
+++ src/components/todo/AddTodo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+	Title,
+	Container,
+	TodoContainer,
+	StyledInput,
+	CheckBox,
+	StyledButton,
+} from '../../styles/todo/AddTodo.styles';
+
+export default function AddTodo() {
+	return (
+		<Container>
+			<Title>카테고리 1</Title>
+			<TodoContainer>
+				<CheckBox type="checkbox" />
+				<StyledInput type="text" placeholder="할 일 입력" />
+				<StyledButton>추가</StyledButton>
+			</TodoContainer>
+		</Container>
+	);
+}
---
+++ src/styles/todo/AddTodo.styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	background-color: white;
+	padding: 1rem;
+	border-radius: 10px;
+	margin: 1rem;
+`;
+
+export const Title = styled.button`
+	font-size: 1rem;
+	background-color: rgb(220, 220, 220);
+	padding: 0.5rem 1rem;
+	border-radius: 3rem;
+	font-weight: bold;
+	width: auto;
+`;
+
+export const TodoContainer = styled.div`
+	margin-top: 1rem;
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
+`;
+
+export const CheckBox = styled.input`
+	appearance: none;
+	width: 1.2rem;
+	height: 1.2rem;
+	border: 1.5px solid gainsboro;
+	border-radius: 0.35rem;
+
+	&:checked {
+		border-color: transparent;
+		background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
+		background-color: #222222;
+	}
+`;
+
+export const StyledInput = styled.input`
+	flex: 1;
+	max-width: 100%;
+	padding: 5px;
+	margin-left: 10px;
+	border: none;
+	border-bottom: 1px solid black;
+`;
+
+export const StyledButton = styled.button`
+	padding: 0.3rem;
+	margin-left: 0.5rem;
+	background-color: #222222;
+	color: white;
+	border: none;
+	border-radius: 5px;
+`;
