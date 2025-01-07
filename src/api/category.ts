e81a091592652932247b42791b6e5b2d27ca9d8e
현지현 <qwere4585@naver.com>
2025-01-07 15:19:02 +0900

    feat: 카테고리 조회 api 연결

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,18 +1,37 @@
-import axios from "axios"
+import axios from 'axios';
 
-export const registerCategory = async ( userId, categoryData ) => {
-    try {
-        const response = await axios.post(
-            `http://localhost:4000/api/todo/category/${userId}`,
-            categoryData,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        console.log(response.data);
-    } catch (error) {
-        console.error("Error registering category:", error);
-    }
+type CategoryData = {
+  categoryName: string;
+  color: string;
+};
+
+export const registerCategory = async (
+  userId: string,
+  categoryData: CategoryData,
+) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:4000/api/todo/category/${userId}`,
+      categoryData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error registering category:', error);
+  }
+};
+
+export const fetchCategories = async (userId: string) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:4000/api/todo/category/${userId}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching category:', error);
+  }
 };
--- src/pages/todo/category/CategoryPage.tsx
+++ src/pages/todo/category/CategoryPage.tsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from 'react';
 import CategoryHeader from '../../../components/todo/category/CategoryHeader';
 import MainLayout from '../../../layout/MainLayout';
 import * as S from '../../../styles/todo/category/Category.style';
+import { useUserStore } from '../../../store/userStore';
+import { fetchCategories } from '../../../api/category';
 
 const CategoryPage = () => {
-	return (
-		<MainLayout>
-			<CategoryHeader title="카테고리" button="+" />
-			<S.CategoryList>
-				<S.CategoryItem>개인 학습</S.CategoryItem>
-				<S.CategoryItem>중요 일정</S.CategoryItem>
-				<S.CategoryItem>약속</S.CategoryItem>
-			</S.CategoryList>
-		</MainLayout>
-	);
+  const { userId } = useUserStore((state) => state);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      if (userId) {
+        try {
+          const categoryData = await fetchCategories(userId);
+          setCategories(categoryData);
+        } catch (error) {
+          console.error('Failed to load category data:', error);
+        }
+      }
+    };
+    loadCategories();
+  }, [userId]);
+
+  return (
+    <MainLayout>
+      <CategoryHeader title="카테고리" button="+" />
+      <S.CategoryList>
+        {categories.map((category, index) => (
+          <S.CategoryItem key={index} color={category.color}>{category.categoryName}</S.CategoryItem>
+        ))}
+      </S.CategoryList>
+    </MainLayout>
+  );
 };
 
 export default CategoryPage;
