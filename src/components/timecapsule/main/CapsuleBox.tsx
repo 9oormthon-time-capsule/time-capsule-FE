cf1a44937dc556b242fbfe405af1981fca34a724
jihyeon <105184159+Jihyeoniiiii@users.noreply.github.com>
2025-01-07 10:51:27 +0900

    [FEAT] feat: 편지 보관 개수 api 연결 (#39)
    * feat: 편지 개수 api 연결
    
    * feat: 편지 개수 이미지 변경 로직 추가
    
    * feat: 보관함 이동 경로 수정

--- src/components/timecapsule/main/CapsuleBox.tsx
+++ src/components/timecapsule/main/CapsuleBox.tsx
@@ -1,24 +1,39 @@
+import { useParams } from 'react-router-dom';
 import * as S from '../../../styles/timecapsule/main/CapsuleBox.style';
 
-const CapsuleBox = () => {
-	const handleCapsuleBox = () => {
-		window.location.href = '/directory/time/1';
-	};
+const IMAGE_MAP = [
+	{ range: [1, 3], src: '/main/Box-2.png' },
+	{ range: [4, 8], src: '/main/Box-3.png' },
+	{ range: [9, 12], src: '/main/Box-4.png' },
+  ];
 
-	return (
-		<S.CapsuleBoxContainer>
-			<S.TitleContainer>
-				<S.TopSection>2026년의 나에게</S.TopSection>
-				<S.BottomSection>
-					<img src="/main/Capsule.svg" width={50} height={35} />
-					<span>0</span>개의 편지가 보관되어있어요!
-				</S.BottomSection>
-			</S.TitleContainer>
-			<S.BoxContainer onClick={handleCapsuleBox}>
-				<img src="/main/Box-1.png" width={450} height={400} />
-			</S.BoxContainer>
-		</S.CapsuleBoxContainer>
-	);
+const CapsuleBox = ({ letterCount }) => {
+  const param = useParams();
+  const handleCapsuleBox = () => {
+    window.location.href = `/directory/time/${param.id}`;
+  };
+
+  const getImageSrc = () => {
+    const found = IMAGE_MAP.find(
+      (item) => letterCount >= item.range[0] && letterCount <= item.range[1]
+    );
+    return found ? found.src : '/main/Box-1.png';
+  };
+
+  return (
+    <S.CapsuleBoxContainer>
+      <S.TitleContainer>
+        <S.TopSection>2026년의 나에게</S.TopSection>
+        <S.BottomSection>
+          <img src="/main/Capsule.svg" width={50} height={35} />
+          <span>{letterCount}</span>개의 편지가 보관되어있어요!
+        </S.BottomSection>
+      </S.TitleContainer>
+      <S.BoxContainer onClick={handleCapsuleBox}>
+        <img src={getImageSrc()} width={470} height={470} />
+      </S.BoxContainer>
+    </S.CapsuleBoxContainer>
+  );
 };
 
 export default CapsuleBox;
