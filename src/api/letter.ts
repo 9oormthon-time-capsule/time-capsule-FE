1045c5c9b078a9465ef89d688b3c2da88c117cc9
현지현 <qwere4585@naver.com>
2025-01-07 14:55:14 +0900

    refactor: 응답 테스트 로그 제거

--- src/api/letter.ts
+++ src/api/letter.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const fetchLetterCount = async (userId: string) => {
     try {
         const response = await axios.get(`http://localhost:4000/api/timecapsule/letter/${userId}`);
-        console.log(response)
 
         return response.data.length;
     } catch (error) {
