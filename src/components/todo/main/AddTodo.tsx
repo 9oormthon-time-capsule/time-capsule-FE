536bb340dc48f9c8ccc485250baa9b62e0a8b600
kkyung99 <becky0508@naver.com>
2025-01-08 22:56:06 +0900

    feat: 카테고리 클릭 시 입력창 토글 기능 및 스타일 수정

--- src/components/todo/main/AddTodo.tsx
+++ src/components/todo/main/AddTodo.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import * as S from '../../../styles/todo/main/AddTodo.style';
+import { fetchCategories } from '../../../api/category';
+
+type category = {
+  id: string;
+  categoryName: string;
+  textColor: string;
+};
 
 export default function AddTodo() {
+  const [categories, setCategories] = useState<category[]>([]);
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const data = await fetchCategories();
+        setCategories(data);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    getCategories();
+  }, []);
+
+  const handleCategoryClick = (id: string) => {
+    console.log(id);
+    setActiveCategory((prev) => (prev === id ? null : id));
+  };
+
   return (
     <S.TodoContainer>
-      <S.TodoTitle>카테고리 1</S.TodoTitle>
-      <S.InputGroup>
-        <S.CheckBox type="checkbox" />
-        <S.TodoInput type="text" placeholder="할 일 입력" />
-        <S.AddButton>추가</S.AddButton>
-      </S.InputGroup>
+      <S.CategoryListContainer>
+        {categories.map((category) => (
+          <div>
+            <S.CategoryItem
+              key={category.id}
+              textColor={category.textColor}
+              onClick={() => handleCategoryClick(category.id)}
+            >
+              {category.categoryName}
+              <S.PlusButton>＋</S.PlusButton>
+            </S.CategoryItem>
+            {activeCategory === category.id && (
+              <S.InputGroup>
+                <S.CheckBox type="checkbox" textColor={category.textColor} />
+                <S.TodoInput
+                  type="text"
+                  placeholder="할 일 입력"
+                  textColor={category.textColor}
+                />
+                <S.AddButton textColor={category.textColor}>추가</S.AddButton>
+              </S.InputGroup>
+            )}
+          </div>
+        ))}
+      </S.CategoryListContainer>
     </S.TodoContainer>
   );
 }
