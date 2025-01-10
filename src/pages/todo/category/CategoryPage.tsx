ee45eae31af3607fc1392538edeb456424fc723c
jihyeon <105184159+Jihyeoniiiii@users.noreply.github.com>
2025-01-10 14:22:53 +0900

    [UI] design: 카테고리 삭제 모달창 구현 (#63)
    * feat: 카테고리 삭제 모달창 추가
    
    * refactor: 마운트시 한번만 실행하도록 수정

--- src/pages/todo/category/CategoryPage.tsx
+++ src/pages/todo/category/CategoryPage.tsx
@@ -2,40 +2,48 @@ import { useEffect, useState } from 'react';
 import CategoryHeader from '../../../components/todo/category/CategoryHeader';
 import MainLayout from '../../../layout/MainLayout';
 import * as S from '../../../styles/todo/category/Category.style';
-import { useUserStore } from '../../../store/userStore';
 import { deleteCategory, fetchCategories } from '../../../api/category';
+import CategoryDeleteModal from '../../../components/todo/category/CategoryDeleteModal';
 
 const CategoryPage = () => {
-  const { userId } = useUserStore((state) => state);
   const [categories, setCategories] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const loadCategories = async () => {
-      if (userId) {
         try {
           const categoryData = await fetchCategories();
           setCategories(categoryData);
         } catch (error) {
           console.error('Failed to load category data:', error);
         }
-      }
     };
     loadCategories();
-  }, [userId]);
+  }, []);
 
-  const handleDelete = (id: string) => {
-    if (window.confirm("정말 삭제하시겠습니까?")) {
-      deleteCategory(id)
-        .then(() => {
-          alert("삭제가 완료되었습니다.");
-          setCategories((prevCategories) =>
-            prevCategories.filter((category) => category.id !== id)
-          );
-        })
-        .catch((error) => {
-          console.error("삭제 중 오류 발생:", error);
-          alert("삭제 중 문제가 발생했습니다.");
-        });
+  const handleDeleteClick = (id: string) => {
+    setIsModalOpen(true);
+    setSelectedCategoryId(id);
+  }
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setSelectedCategoryId(null);
+  }
+
+  const handleConfirmDelete = async () => {
+    if (!selectedCategoryId) return;
+    try {
+      await deleteCategory(selectedCategoryId);
+      setCategories((prevCategories) =>
+        prevCategories.filter((category) => category.id !== selectedCategoryId)
+      );
+    } catch (error) {
+      console.error('삭제 중 오류 발생:', error);
+      alert('삭제 중 문제가 발생했습니다.');
+    } finally {
+      handleModalClose();
     }
   };
 
@@ -45,7 +53,7 @@ const CategoryPage = () => {
       <S.CategoryList>
         {Array.isArray(categories) && categories.length > 0 ? (
           categories.map((category, index) => (
-            <S.CategoryItem key={index} textColor={category.textColor} onClick={() => handleDelete(category.id)}>
+            <S.CategoryItem key={index} textColor={category.textColor} onClick={() => handleDeleteClick(category.id)}>
               {category.categoryName}
             </S.CategoryItem>
           ))
@@ -54,8 +62,15 @@ const CategoryPage = () => {
             등록된 카테고리가 없습니다. <br />
             새 카테고리를 추가해보세요!
           </S.EmptyMessage>
-        )}
+        )
+      }
       </S.CategoryList>
+      {isModalOpen && (
+        <CategoryDeleteModal
+          onClose={handleModalClose}
+          onConfirm={handleConfirmDelete}
+        />
+      )}
     </MainLayout>
   );
 };
