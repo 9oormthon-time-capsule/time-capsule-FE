4c14b1882cf117d1545c9d1617a54f8617fe6061
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-01-11 18:32:28 +0900

    feat: 편지 작성 제한 기능 추가 (#74)

--- src/api/letter.ts
+++ src/api/letter.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+interface ILetter {
+  createdAt: { seconds: number };
+}
+
 export const fetchLetterCount = async () => {
   try {
     const response = await axios.get(
@@ -19,13 +23,49 @@ export const fetchLetterCount = async () => {
 export const submitLetter = async (letter: string) => {
   try {
     const response = await axios.post(
-      'http://localhost:4000/api/timecapsule/letter', 
-      { content: letter },  
-      { withCredentials: true }
+      'http://localhost:4000/api/timecapsule/letter',
+      { content: letter },
+      { withCredentials: true },
     );
     console.log(response.data);
   } catch (error) {
     console.error('Error submitting letter:', error);
     throw error;
+  }
+};
+
+export const canWriteLetter = async () => {
+  try {
+    const response = await axios.get(
+      'http://localhost:4000/api/timecapsule/letter',
+      {
+        withCredentials: true,
+      },
+    );
+
+    if (response.data.length === 0) {
+      return true;
+    }
+
+    const sortedLetters = response.data
+      .sort(
+        (
+          a: { createdAt: { seconds: number } },
+          b: { createdAt: { seconds: number } },
+        ) => b.createdAt.seconds - a.createdAt.seconds,
+      )
+      .map((item: ILetter) => new Date(item.createdAt.seconds * 1000));
+
+    const latestLetterDate = sortedLetters[0];
+    const latestLetterMonth = latestLetterDate.getMonth();
+    const currentMonth = new Date().getMonth();
+
+    if (latestLetterMonth === currentMonth) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.error('편지 작성 제한 체크 중 오류가 발생했습니다:', error);
+    return false;
   }
 };
--- src/components/timecapsule/main/LetterMenuOverlay.tsx
+++ src/components/timecapsule/main/LetterMenuOverlay.tsx
@@ -1,20 +1,28 @@
+import { canWriteLetter } from '../../../api/letter';
 import * as S from '../../../styles/timecapsule/main/LetterMenuOverlay.style';
 
 const LetterMenuOverlay = () => {
-	const handleNavigation = (path: string) => {
-		window.location.href = path;
-	};
+  const handleNavigation = async (path: string) => {
+    if (path === '/write/letter') {
+      const canWrite = await canWriteLetter();
+      if (!canWrite) {
+        alert('이번 달 편지를 작성하셨네요. 다음 달에 작성해주세요!');
+        return;
+      }
+    }
+    window.location.href = path;
+  };
 
-	return (
-		<S.OverlayContainer>
-			<S.OverlayContent onClick={() => handleNavigation('/write/letter')}>
-				2026년의 나에게 편지 작성하기
-			</S.OverlayContent>
-			<S.OverlayContent onClick={() => handleNavigation('/write/reflect')}>
-				오늘의 일기 작성하기
-			</S.OverlayContent>
-		</S.OverlayContainer>
-	);
+  return (
+    <S.OverlayContainer>
+      <S.OverlayContent onClick={() => handleNavigation('/write/letter')}>
+        2026년의 나에게 편지 작성하기
+      </S.OverlayContent>
+      <S.OverlayContent onClick={() => handleNavigation('/write/reflect')}>
+        오늘의 일기 작성하기
+      </S.OverlayContent>
+    </S.OverlayContainer>
+  );
 };
 
 export default LetterMenuOverlay;
