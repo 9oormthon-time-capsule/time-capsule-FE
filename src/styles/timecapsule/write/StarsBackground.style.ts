2d4f8d742d5f84df1e263152feb7eca7f420be45
kkyung99 <becky0508@naver.com>
2025-01-11 14:39:57 +0900

    design: 편지와 회고 작성 페이지 UI 개선

--- src/styles/timecapsule/write/StarsBackground.style.ts
+++ src/styles/timecapsule/write/StarsBackground.style.ts
@@ -8,30 +8,34 @@ const blinkAnimation = keyframes`
 `;
 
 export const StarsBackgroundContainer = styled.div`
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background: linear-gradient(rgb(134, 138, 169), #e8b9a4);
-	overflow: hidden;
-	z-index: -1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#340b0d, #bf6b4b);
+  /* background: linear-gradient( #4A2C16, #A05A2C, #E1A95F); */
+  /* background: linear-gradient(#2e1a0f, #a45c40, #ffc373); */
+  background: linear-gradient(#2a1300, #7f4326, #daa85a);
+
+  overflow: hidden;
+  z-index: -1;
 `;
 
 interface StarProps {
-	animationDuration?: string;
-	opacity?: number;
-	animationDelay?: string;
+  animationDuration?: string;
+  opacity?: number;
+  animationDelay?: string;
 }
 
 export const Star = styled.div<StarProps>`
-	position: absolute;
-	width: 2px;
-	height: 2px;
-	background-color: white;
-	border-radius: 50%;
-	animation: ${blinkAnimation}
-		${({ animationDuration }) => animationDuration || '2s'} infinite ease-in-out;
-	opacity: ${({ opacity }) => opacity || 1};
-	animation-delay: ${({ animationDelay }) => animationDelay || '0s'};
+  position: absolute;
+  width: 2px;
+  height: 2px;
+  background-color: white;
+  border-radius: 50%;
+  animation: ${blinkAnimation}
+    ${({ animationDuration }) => animationDuration || '2s'} infinite ease-in-out;
+  opacity: ${({ opacity }) => opacity || 1};
+  animation-delay: ${({ animationDelay }) => animationDelay || '0s'};
 `;
--- src/styles/timecapsule/write/WriteForm.style.ts
+++ src/styles/timecapsule/write/WriteForm.style.ts
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 export const FormContainer = styled.form`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin-top: 1rem;
-	height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+  height: auto;
 `;
 
 export const ContentTextArea = styled.textarea`
-	width: 60%;
-	height: 70vh;
-	padding: 1rem;
-	border: 1px solid #ccc;
-	border-radius: 20px;
-	resize: none;
-	font-size: 16px;
-	line-height: 1.5;
-	margin-bottom: 2rem;
-	box-shadow: rgba(0, 0, 0, 0.3) 0px 4px 50px;
+  width: 60%;
+  height: 50vh;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  resize: none;
+  font-size: 1.2rem;
+  line-height: 1.5;
+  margin-bottom: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 4px 50px;
 `;
