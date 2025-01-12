3c6cb17057daa3eb55ced44f9d3981859cb16aa0
sohee <106158901+ccconac@users.noreply.github.com>
2025-01-12 22:32:56 +0900

    [FEAT] feat: 타임캡슐 상세 페이지 API 연결 (#85)
    * feat: 일일 회고 상세 페이지 API 연결
    
    * feat: 타임 캡슐 상세 페이지 API 연결

--- src/pages/timecapsule/detail/LetterDetail.jsx
+++ src/pages/timecapsule/detail/LetterDetail.jsx
@@ -1,43 +1,52 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import * as S from '../../../styles/timecapsule/detail/LetterDetail.style';
+import { fetchLetterData } from '../../../api/directoryLetter';
 
 const LetterDetail = () => {
-	const location = useLocation();
-	const { letterContent } = location.state || {
-		letterContent: {
-			to: '2026년의 나에게 보내는 편지',
-			body: '미래의 나에게 보내는 메시지를 여기에 작성했습니다. 1년 동안 많은 일들이 있었겠지요?',
-			from: '2025년의 내가 보내는 편지',
-		},
-	};
+  const { letterId } = useParams();
+  const location = useLocation();
+  const [letterData, setLetterData] = useState([]);
 
-	const handleDownload = () => {
-		const input = document.getElementById('letter');
-		html2canvas(input).then((canvas) => {
-			const imgData = canvas.toDataURL('image/png');
-			const pdf = new jsPDF();
-			pdf.addImage(imgData, 'PNG', 10, 10, 190, 0);
-			pdf.save('letter-detail.pdf');
-		});
-	};
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchLetterData('타임캡슐');
 
-	return (
-		<S.LetterDetailContainer>
-			<S.BackButton onClick={() => window.history.back()}>&larr;</S.BackButton>
-			<S.Title>💌작년 1월의 내가 미래의 나에게 보내온 편지💌</S.Title>
-			<S.LetterContent id="letter">
-				<S.ToText>To. {letterContent.to}</S.ToText>
-				<S.BodyText>{letterContent.body}</S.BodyText>
-				<S.FromText>from. {letterContent.from}</S.FromText>
-			</S.LetterContent>
-			<S.DownloadButton onClick={handleDownload}>
-				📥 PDF로 다운로드
-			</S.DownloadButton>
-		</S.LetterDetailContainer>
-	);
+      const selectedLetter = data.find((letter) => letter.id === letterId);
+      setLetterData(selectedLetter || null);
+    };
+
+    loadData();
+  }, [letterId]);
+
+  console.log(letterData);
+
+  const handleDownload = () => {
+    const input = document.getElementById('letter');
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, 'PNG', 10, 10, 190, 0);
+      pdf.save('letter-detail.pdf');
+    });
+  };
+
+  return (
+    <S.LetterDetailContainer>
+      <S.BackButton onClick={() => window.history.back()}>&larr;</S.BackButton>
+      <S.Title>
+        💌 2025년 {letterData.createdAt}의 내가 미래의 나에게 보낸 편지 💌
+      </S.Title>
+      <S.LetterContent id="letter">
+        <S.BodyText>{letterData.content}</S.BodyText>
+      </S.LetterContent>
+      <S.DownloadButton onClick={handleDownload}>
+        📥 PDF로 다운로드
+      </S.DownloadButton>
+    </S.LetterDetailContainer>
+  );
 };
 
 export default LetterDetail;
