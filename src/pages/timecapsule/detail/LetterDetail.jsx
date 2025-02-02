d1385b429619412883300f279867d84de4774ca5
*jimin* <*jimin*>
2025-02-02 17:59:00 +0900

    feat: LetterDetail 코드 수정

--- src/pages/timecapsule/detail/LetterDetail.jsx
+++ src/pages/timecapsule/detail/LetterDetail.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import * as S from '../../../styles/timecapsule/detail/LetterDetail.style';
 
 import { StarsBackground } from '../../../components/timecapsule/write/StarsBackground';
 
+import { fetchLetterCount } from '../../../api/letter';
+
 const LetterDetail = () => {
 	const location = useLocation();
-	const { letterContent } = location.state || {
-		letterContent: {
-			to: '2026년의 나에게 보내는 편지',
-			body: '미래의 나에게 보내는 메시지를 여기에 작성했습니다. 1년 동안 많은 일들이 있었겠지요?',
-			from: '2025년의 내가 보내는 편지',
-		},
-	};
+	
+	const { letterId } = useParams();
+	const [letterContent, setLetterContent] = useState(null);
+
+	useEffect(() => {
+		const getLetterContent = async () => {
+		  try {
+			const content = await fetchLetterContent(letterId);
+			console.log('Fetched Letter Content:', content);
+			setLetterContent(content);
+		  } catch (error) {
+			console.error('Error fetching letter content:', error);
+		  }
+		};
+		
+		getLetterContent();
+	  }, [letterId]);
 
 	const handleDownload = () => {
 		const input = document.getElementById('letter');
@@ -26,17 +38,24 @@ const LetterDetail = () => {
 		});
 	};
 
+	if (!letterContent) return <div>Loading...</div>;
+
 	return (
 		<S.LetterDetailContainer>
 			<StarsBackground />
 
-			<S.BackButton onClick={() => window.history.back()}>&larr;</S.BackButton>
-			<S.Title>💌작년 1월의 내가 미래의 나에게 보내온 편지💌</S.Title>
+			<S.BackButton onClick={() => window.history.back()}>
+				&larr;
+			</S.BackButton>
+
+			<S.Title>
+				💌작년 1월의 내가 미래의 나에게 보내온 편지💌
+			</S.Title>
+
 			<S.LetterContent id="letter">
-				<S.ToText>To. {letterContent.to}</S.ToText>
 				<S.BodyText>{letterContent.body}</S.BodyText>
-				<S.FromText>from. {letterContent.from}</S.FromText>
 			</S.LetterContent>
+
 			<S.DownloadButton onClick={handleDownload}>
 				📥 PDF로 다운로드
 			</S.DownloadButton>
