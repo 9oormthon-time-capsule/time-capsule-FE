593f223ef4dcad97955429cc9255490c83f7452e
sohee <106158901+ccconac@users.noreply.github.com>
2025-02-17 17:32:48 +0900

    [REFACTOR] refactor: “/” 라우트로 이동할 때의 UI 개선 (#146)

--- src/components/common/PrivateRoute.tsx
+++ src/components/common/PrivateRoute.tsx
@@ -1,11 +1,17 @@
 import { Navigate } from 'react-router-dom';
 
+interface IRouteElement {
+  element: JSX.Element;
+}
+
 const isLoggedIn = () => {
   return localStorage.getItem('user-info') !== null;
 };
 
-const PrivateRoute = ({ element }: { element: JSX.Element }) => {
+export const PrivateRoute = ({ element }: IRouteElement) => {
   return isLoggedIn() ? element : <Navigate to="/" />;
 };
 
-export default PrivateRoute;
+export const PublicRoute = ({ element }: IRouteElement) => {
+  return isLoggedIn() ? <Navigate to="/main" /> : element;
+};
