0d84c94de068cce4db02a0acacdfff89da3da51a
현지현 <qwere4585@naver.com>
2025-02-18 16:49:24 +0900

    design: 이모지 선택 ui 수정

--- src/styles/timecapsule/write/WritePage.style.ts
+++ src/styles/timecapsule/write/WritePage.style.ts
@@ -70,6 +70,7 @@ interface EmotionLabelProps {
 export const EmotionLabel = styled.label<EmotionLabelProps>`
   font-size: 1.8rem;
   margin: 5px;
+  padding: 4px 6px 0 6px;
   cursor: pointer;
   background-color: ${({ isSelected }) => (isSelected ? '#5C271F' : '')};
   transition: background-color 0.3s;
