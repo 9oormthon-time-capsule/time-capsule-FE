567f8375ac473711cd04a2c2c2aa7e4b2f7aa2f1
kkyung99 <becky0508@naver.com>
2025-02-18 15:45:04 +0900

    chore: 헤더, 알림창 오타 및 불필요한 문자 수정

--- src/components/todo/category/CategoryModal.tsx
+++ src/components/todo/category/CategoryModal.tsx
@@ -63,7 +63,7 @@ const CategoryModal: React.FC<CategoryModalProps> = ({
           <S.MainMessage>
             해당 카테고리를 삭제하시겠습니까?
             <br />
-            "포함되어 있던 할 일들은 모두 삭제됩니다."
+            포함되어 있던 할 일들은 모두 삭제됩니다.
           </S.MainMessage>
         )}
         <S.ButtonGroup>
