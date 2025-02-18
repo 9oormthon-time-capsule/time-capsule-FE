424d7e1bcbab90f462744c5ce47a2b920ed14130
ccconac <qwerpoiu91@naver.com>
2025-02-18 17:32:54 +0900

    feat: 디렉터리 비어 있을 때 작성 페이지로 라우팅 유도

--- src/components/timecapsule/directory/Capsules.tsx
+++ src/components/timecapsule/directory/Capsules.tsx
@@ -1,6 +1,7 @@
 import * as S from '../../../styles/timecapsule/directory/Directory.style';
 import { CAPSULE_IMAGE } from '../../../mock/capsule';
 import { canReadLetter } from '../../../api/letter';
+import EmptyDirectory from './EmptyDirectory';
 
 interface ICapsuleContainer {
   letterData: { id: number; createdAt: string }[];
@@ -21,25 +22,38 @@ const CapsuleContainer = ({ letterData, pageType }: ICapsuleContainer) => {
       window.location.href = `/detail/reflect/${dataId}`;
     }
   };
+
   return (
-    <S.CapsuleContainer>
-      {letterData.map((data, index) => (
-        <S.CapsuleBox key={data.id}>
-          <S.CapsuleLabelBox>
-            <S.CapsuleLabel>{data.createdAt}</S.CapsuleLabel>
-          </S.CapsuleLabelBox>
-          <a
-            href="#!"
-            onClick={(e) => {
-              e.preventDefault();
-              handleClick(data.id);
-            }}
-          >
-            <S.CapsuleImg src={CAPSULE_IMAGE[index % CAPSULE_IMAGE.length]} />
-          </a>
-        </S.CapsuleBox>
-      ))}
-    </S.CapsuleContainer>
+    <>
+      {letterData.length ? (
+        <S.CapsuleContainer>
+          {letterData.map((data, index) => (
+            <S.CapsuleBox key={data.id}>
+              <S.CapsuleLabelBox>
+                <S.CapsuleLabel>{data.createdAt}</S.CapsuleLabel>
+              </S.CapsuleLabelBox>
+              <a
+                href="#!"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleClick(data.id);
+                }}
+              >
+                <S.CapsuleImg
+                  src={CAPSULE_IMAGE[index % CAPSULE_IMAGE.length]}
+                />
+              </a>
+            </S.CapsuleBox>
+          ))}
+        </S.CapsuleContainer>
+      ) : (
+        <EmptyDirectory
+          directoryType={pageType === '타임캡슐' ? '타입캡슐이' : '일일 회고가'}
+          writeType={pageType}
+          href={pageType === '타임캡슐' ? '/write/letter' : '/write/reflect'}
+        />
+      )}
+    </>
   );
 };
 
